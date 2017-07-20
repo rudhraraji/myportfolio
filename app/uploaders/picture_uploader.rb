48f98b8500ec659dd02931fdcf79d00c79ba9121
rudhraraji <rudhraraji@gmail.com>
2017-07-20 23:08:54 +1000

    Images from s3 bucket

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,18 +1,21 @@
+require 'carrierwave'
+
 class PictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+   # include CarrierWave::MiniMagick
+   # process resize_to_limit: [400, 400]
 
   # Choose what kind of storage to use for this uploader:
   #storage :file
-  # storage :fog
+   storage :fog
 
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  # if Rails.env.production?
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
@@ -20,6 +23,10 @@ class PictureUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
@@ -42,9 +49,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_whitelist
-    %w(jpg jpeg gif png)
-  end
+  
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,17 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
+  	config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       # Configuration for Amazon S3
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :region   			 => 'ap-southeast-2',
     }
     config.fog_directory     =  ENV['S3_BUCKET']
   end
 end
+
+#AccessKey = AKIAJPDLVCC7RNBTLISA
+#SecretKey = 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
+#S3Bucket  = rudhraraji
